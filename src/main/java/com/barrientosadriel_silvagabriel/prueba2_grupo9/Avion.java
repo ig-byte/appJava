f13cfb90ae2c9973d0adf02daec2ed0fe1b65b1b
Adriel Barrientos <adrielgbm@gmail.com>
2023-10-02 22:44:50 -0300

    Corrections in Avion

--- src/main/java/com/barrientosadriel_silvagabriel/prueba2_grupo9/Avion.java
+++ src/main/java/com/barrientosadriel_silvagabriel/prueba2_grupo9/Avion.java
@@ -20,13 +20,18 @@ public class Avion {
     }
 
     public void anularVuelo(int vueloAnulado) {
-        if (vueloAnulado > 0 && vueloAnulado < 43) {
+        vueloAnulado--;
+        if (vueloAnulado >= 0 && vueloAnulado < 42) {
             if (!asientosPasajeros[vueloAnulado].isEmpty()) {
                 asientosPasajeros[vueloAnulado].pasajero = null;
-                System.out.println("El vuelo se ha anulado.");
-            } else
-                System.out.println("El vuelo está vacío.");
+                System.out.println("El vuelo se ha anulado");
+            } else {
+                System.out.println("El vuelo no puede ser anulado");
+                System.out.println("El asiento se encuentra disponible");
+            }
         }
+        else
+            System.out.println("Asiento inexistente");
     }
 
     public void comprarPasaje(int asiento) throws IOException {
@@ -36,10 +41,10 @@ public class Avion {
         boolean vip = false, check = false;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        if (asiento > 0 && asiento < 43) {
-            if (!asientosPasajeros[asiento].isEmpty()) {
-                System.out.println("Asiento no disponible");
-                if (!asientosPasajeros[asiento].isVip()) {
+        asiento--;
+        if (asiento >= 0 && asiento < 42) {
+            if (asientosPasajeros[asiento].isEmpty()) {
+                if (asientosPasajeros[asiento].isVip()) {
                     System.out.println("Asiento Premium VIP disponible");
                     valor = 240000;
                 }
@@ -55,7 +60,7 @@ public class Avion {
                 System.out.print("\nIngrese el telefono (ej: 912345678): ");
                 telefono = Integer.parseInt(br.readLine());
                 do {
-                    System.out.println("Usuario VIP:");
+                    System.out.println("\nUsuario VIP:");
                     System.out.println("1.- Si");
                     System.out.println("2.- No");
                     System.out.print("Ingrese su opción: ");
@@ -76,15 +81,18 @@ public class Avion {
                             break;
                     }
                 } while (!check);
+                System.out.println("Valor del vuelo: $" + (valor * descuento));
+                System.out.println(".");
+                System.out.println("..");
+                System.out.println("...");
+                asientosPasajeros[asiento].pasajero = new Pasajero(nombre, rut, telefono, vip);
+                System.out.println("Vuelo comprado con exito");
             }
-            
-            System.out.println("Valor del vuelo: $" + (valor * descuento));
-            System.out.println(".");
-            System.out.println("..");
-            System.out.println("...");
-            asientosPasajeros[asiento].pasajero = new Pasajero(nombre, rut, telefono, vip);
-            System.out.println("Vuelo comprado con exito.");
+            else
+                System.out.println("Asiento no disponible");
         }
+        else
+            System.out.println("Asiento inexistente");
     }
 
     public void mostrarAsientos() {
@@ -92,13 +100,23 @@ public class Avion {
         String[] au = new String[42];
         for (int i = 0; i < asientosPasajeros.length; i++) {
             if (asientosPasajeros[i].pasajero == null)
-                aux = Integer.toString(i);
+                aux = Integer.toString(i + 1);
             else if (i > 10)
                 aux = "x ";
             else
                 aux = "x";
             au[i] = aux;
         }
+        System.out.println("|  " + au[0] + "   " + au[1] + "   " + au[2] + "   " + au[3] + "   " + au[4] + "   " + au[5] + "  |");
+        System.out.println("|  " + au[6] + "   " + au[7] + "   " + au[8] + "   " + au[9] + "  " + au[10] + "  " + au[11] + " |");
+        System.out.println("|  " + au[12] + "  " + au[13] + "  " + au[14] + "  " + au[15] + "  " + au[16] + "  " + au[17] + " |");
+        System.out.println("|  " + au[18] + "  " + au[19] + "  " + au[20] + "  " + au[21] + "  " + au[22] + "  " +au[23] +" |");
+        System.out.println("|  " + au[24] + "  " + au[25] + "  " + au[26] + "  " + au[27] + "  " + au[28] + "  " +au[29] +" |");
+        System.out.println("|____________  ___________|");
+        System.out.println("|____________  ___________|");
+        System.out.println("|  "+au[30]+"  "+au[31]+"  "+au[32]+"  "+au[33]+"  "+au[34]+"  "+au[35]+" |");
+        System.out.println("|  "+au[36]+"  "+au[37]+"  "+au[38]+"  "+au[39]+"  "+au[40]+"  "+au[41]+" |");
+
     }
 
     @Override
